f7f13a6477448ba89afce744b63f6034294b579e
unknown <pshaha712@gmail.com>
2020-03-01 17:54:45 +0530

    added sorting functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { HttpClient} from '@angular/common/http'
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,8 @@ export class AppComponent {
   isDetails = false ;
   totalUser = 0 ;
   users;
+  selectedOpn = -1;
+  dataOfUser = [];
   constructor(private http : HttpClient) {
 
   }
@@ -22,7 +25,7 @@ export class AppComponent {
 
   searchUser(searchText) {
     console.log('searchText', searchText);
-    this.http.get("https://api.github.com/search/users?q=" + searchText)
+    this.http.get("https://api.github.com/search/users" + '?q=' + searchText)
     .subscribe(data => {
       console.log('data', data);
       this.userData = data;
@@ -38,9 +41,40 @@ export class AppComponent {
     this.userDetail = [];
     this.http.get("https://api.github.com/users/" + userName +"/repos")
     .subscribe(data => {
-      this.isDetails = true;
+      this.users[index]['isDetails'] = true ;
+      //this.isDetails = true;
       console.log('user details data', data);
       this.userDetail = data;
     })
+  }
+
+  sortUser(event: any) {
+    this.selectedOpn = event.target.value;
+    console.log("selected option", this.selectedOpn);
+    console.log('usorted array',this.users);
+ 
+    if(this.selectedOpn == 1) {
+      console.log('in ato z if');
+      this.users.sort(function(a, b){
+        var nameA=a.login.toLowerCase(), nameB=b.login.toLowerCase()
+        if (nameA < nameB) //sort string ascending
+            return -1 
+        if (nameA > nameB)
+            return 1
+        return 0 //default return value (no sorting)
+    })
+    }
+     else if(this.selectedOpn == 2) {
+       console.log('in z to a if');
+      this.users.sort(function(a, b){
+        var nameA=a.login.toLowerCase(), nameB=b.login.toLowerCase()
+        if (nameA < nameB) //sort string descending
+            return 1 
+        if (nameA > nameB)
+            return -1
+        return 0 //default return value (no sorting)
+    })
+     }
+  console.log('sorted array', this.users);
   }
 }
