d7525db6ebea608714c91158cf22538564d636f6
unknown <pshaha712@gmail.com>
2020-03-02 21:40:59 +0530

    Added spinner and code refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -19,53 +18,64 @@ export class AppComponent {
   p = 1;
   count = 5;
   datalength = 0;
+  isDataLoaded = true ;
   constructor(private http : HttpClient) {
 
   }
 
   ngOnInit() {
-  }
-
-  searchUser(searchText) {
-    console.log('searchText', searchText);
-    this.http.get("https://api.github.com/search/users" + '?q=' + searchText)
+    this.http.get('https://api.github.com/users')
     .subscribe(data => {
-      console.log('data', data);
-      this.userData = data;
-      this.users= this.userData.items;
-      this.datalength = this.users.length;
-      this.totalUser = this.userData.total_count;
+        this.users = data;
+        this.datalength = this.users.length;
+        this.totalUser = this.datalength;
+        this.isDataLoaded = false ;
     })
   }
 
-  userDetails(index) {
+  searchUser(searchText) {
+    this.p = 1
+    this.isDataLoaded = true;
+    this.userData = [];
+    this.users = [];
+    this.datalength = 0 ;
+    this.totalUser = 0;
+    this.http.get("https://api.github.com/search/users" + '?q=' + searchText)
+    .subscribe(data => {
+      this.userData = data;
+      this.users= this.userData.items;
+      console.log('userData', this.users);
+      this.datalength = this.users.length;
+      this.totalUser = this.userData.total_count;
+      this.isDataLoaded = false ;
+    })
+  }
+
+  userDetails(userName) {
     for(let i=0; i<this.users.length ; i++) {
-      console.log('in for loop');
       if(this.users[i].isDetails) {
-        console.log('in if method');
         this.users[i]['isDetails'] = false ;
       }
     }
     this.isDetails = false ;
-    let userName = this.users[index].login ;
-    console.log('in detalis method',index);
     this.userDetail = [];
     this.http.get("https://api.github.com/users/" + userName +"/repos")
     .subscribe(data => {
-      this.users[index]['isDetails'] = true ;
-      //this.isDetails = true;
-      console.log('user details data', data);
+      this.users.forEach(user => {
+        if(user.login == userName)
+        {
+          user['isDetails'] = true;
+        }
+      });
       this.userDetail = data;
     })
   }
 
   sortUser(event: any) {
+    this.isDataLoaded = true ;
     this.selectedOpn = event.target.value;
-    console.log("selected option", this.selectedOpn);
-    console.log('usorted array',this.users);
- 
     if(this.selectedOpn == 1) {
-      console.log('in ato z if');
+      this.isDataLoaded = false ;
       this.users.sort(function(a, b){
         var nameA=a.login.toLowerCase(), nameB=b.login.toLowerCase()
         if (nameA < nameB) //sort string ascending
@@ -76,7 +86,7 @@ export class AppComponent {
     })
     }
      else if(this.selectedOpn == 2) {
-       console.log('in z to a if');
+      this.isDataLoaded = false ;
       this.users.sort(function(a, b){
         var nameA=a.login.toLowerCase(), nameB=b.login.toLowerCase()
         if (nameA < nameB) //sort string descending
@@ -86,7 +96,5 @@ export class AppComponent {
         return 0 //default return value (no sorting)
     })
      }
-
-  console.log('sorted array', this.users);
   }
 }
